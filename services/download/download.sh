1ec72a3667bdb3da320dbe314b4085d8d6fe70ad
loy <542124953@qq.com>
2023-06-14 16:37:33 +0800

    modify
    删除无用信息

--- services/download/download.sh
+++ services/download/download.sh
@@ -29,21 +29,6 @@ echo "Downloading, this might take a while..."
 
 aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data --continue
 
-echo "Checking SHAs..."
+git clone https://github.com/Mikubill/sd-webui-controlnet.git /data/config/auto/extensions
 
-parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
-
-cat <<EOF
-By using this software, you agree to the following licenses:
-https://github.com/AbdBarho/stable-diffusion-webui-docker/blob/master/LICENSE
-https://github.com/CompVis/stable-diffusion/blob/main/LICENSE
-https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/LICENSE.txt
-https://github.com/invoke-ai/InvokeAI/blob/main/LICENSE
-https://github.com/sd-webui/stable-diffusion-webui/blob/master/LICENSE
-https://github.com/cszn/BSRGAN/blob/main/LICENSE
-https://github.com/sczhou/CodeFormer/blob/master/LICENSE
-https://github.com/TencentARC/GFPGAN/blob/master/LICENSE
-https://github.com/xinntao/Real-ESRGAN/blob/master/LICENSE
-https://github.com/xinntao/ESRGAN/blob/master/LICENSE
-https://github.com/cszn/SCUNet/blob/main/LICENSE
-EOF
+echo "finish downloading"
